c51998a77f98bcae1b17a4700db107a1bc89e9e7
Luca Di Bello <info@lucadibello.ch>
2024-04-03 17:14:08 +0000

    Now correctly connects elements together

--- modules/models/topology.py
+++ modules/models/topology.py
@@ -1,3 +1,5 @@
+from typing import Sequence
+from modules.util.logger import Logger
 from modules.util.network import Ipv4Network
 
 
@@ -151,25 +153,55 @@ class NetworkTopology:
         self._routers = routers
         self._hosts = hosts
 
-        # TODO: We must ensure that each host is connected to only one router!
+        _total_links = 0
 
+        # Find and create all the links between routers and hosts
+        _total_links += NetworkTopology._create_links(self._routers, self._hosts)
+        # Find links between routers
+        _total_links += NetworkTopology._create_links(self._routers, self._routers)
+        # Find links between hosts
+        _total_links += NetworkTopology._create_links(self._hosts, self._hosts)
+
+        # Check if there are some network elements that are not linked to any other network element
+        for router in self._routers:
+            if not router.get_links():
+                Logger().warning(
+                    f"Router {router.get_name()} is not linked to any other network element."
+                )
+        for host in self._hosts:
+            if not host.get_links():
+                Logger().warning(
+                    f"Host {host.get_name()} is not linked to any other network element."
+                )
+
+        # Save total number of unique links
+        self._total_links = _total_links
+
+    @staticmethod
+    def _create_links(
+        set_a: Sequence["NetworkElement"], set_b: Sequence["NetworkElement"]
+    ) -> int:
         # Find all the links between routers and hosts
         total_links = 0
-        for router in self._routers:
-            for host in self._hosts:
-                # Compute all possible links between the router and the host
-                found, interfaces = _does_link_exist(router, host)
+        for a in set_a:
+            for b in set_b:
+                # If for any reason the two network elements are the same, skip as they cannot be linked
+                if a.get_name() == b.get_name():
+                    continue
+
+                # Compute all possible links between the two network elements
+                found, interfaces = _does_link_exist(a, b)
                 if not found:
                     continue
 
-                # Add the link between the router and the host
+                # Add the link between the two elements
                 for source_interface, destination_interface in interfaces:
                     total_links += 1
-                    router.add_link((host, source_interface))
-                    host.add_link((router, destination_interface))
+                    a.add_link((b, source_interface))
+                    b.add_link((a, destination_interface))
 
-        # Save total number of unique links
-        self._total_links = total_links
+        # Return total amount of unique links
+        return total_links
 
     def __str__(self):
         return f"NetworkTopology(routers={self._routers}, hosts={self._hosts})"
