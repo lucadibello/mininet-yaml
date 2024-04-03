f3918a0b1028286804298d2dabc06593a81f0a91
Luca Di Bello <info@lucadibello.ch>
2024-04-03 13:19:58 +0000

    Working app entrypoint

--- emulation.py
+++ emulation.py
@@ -1,15 +1,30 @@
-from modules import ArgParser
+from modules.args.parser import ArgParser
+from modules.yaml.decoder import decodeTopology
+from modules.util.logger import Logger
 
 
 def main():
-    # Parse arguments
-    args = ArgParser().parse()
+    # Parse + validate arguments
+    args = vars(ArgParser().parse())
 
-    # Print the read arguments
-    print(args)
+    # Extract values
+    file_path: str = args["definition"]
+    must_draw: bool = args["draw"]
+    log_dir: str = args["log"]
 
-    # Now, read and parse the file containing the network definition
+    # If log directory is provided, create the logger with the specified directory
+    if log_dir:
+        Logger(log_dir)
+    Logger().info("Starting the network emulation.")
 
+    Logger().info(f"Reading the network topology from the file: {file_path}")
+
+    # Validate and decode network topology from YAML file
+    topology = decodeTopology(file_path)
+
+    Logger().info(
+        f"Network topology successfully loaded. Found {len(topology.get_routers())} routers and {len(topology.get_hosts())} hosts."
+    )
 
 
 if __name__ == "__main__":
