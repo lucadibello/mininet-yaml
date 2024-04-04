d13cfa8ea6fce2d8bf4fc812b2e48a364a1c955e
Luca Di Bello <info@lucadibello.ch>
2024-04-04 17:19:25 +0000

    Fixed log levels

--- emulation.py
+++ emulation.py
@@ -20,23 +20,23 @@ def main():
 	# Initialize logger singleton with passed settings 
 	Logger(debug=is_verbose) if not log_dir else Logger(log_dir, debug=is_verbose)
 
-	Logger().info(f"Reading the network topology from the file: {file_path}")
+	Logger().debug(f"Reading the network topology from the file: {file_path}")
 
 	# Validate and decode network topology from YAML file
 	topology = decodeTopology(file_path)
 	Logger().info(
-		f"Network topology successfully loaded. Found {len(topology.get_routers())} routers, {len(topology.get_hosts())} hosts and {topology.get_total_links()} unique links."
+		f"Network topology loaded. Found {len(topology.get_routers())} routers, {len(topology.get_hosts())} hosts and {topology.get_total_links()} unique links."
 	)
 
 	# Now, handle what the user wants to do with the network topology
 	if must_draw:
-		Logger().info("Drawing the network topology...")
+		Logger().debug("Drawing the network topology...")
 		graph = topology.draw()
-		Logger().info("Network topology successfully drawn. Output is displayed below.")
+		Logger().info("Topology graph created correctly. Output is displayed below.")
 		# Print the graph to the console
 		print(graph)
 	else:
-		Logger().info("Creating virtual network...")
+		Logger().debug("Creating virtual network...")
 		raise NotImplementedError("The network emulation is not implemented yet.")
 
 
